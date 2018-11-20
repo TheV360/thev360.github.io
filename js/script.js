0b5f8c623866103df3a898f9d7ecc9a0f0c93e15
V360 <v360code@gmail.com>
2018-11-19 23:55:07 -0500

    Fixes and polish

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@
 
 var i;
 var togglenav, nav, navitems;
+var footernote;
+
+const footernotes = [
+	"Sincerely, a Minecraft fan.",
+	"Thank.",
+	"What's happening, fellow gamers?",
+	"This is a randomly chosen footer note.",
+	"This isn't a placeholder!",
+	"Probably not actually copyrighted.",
+	"Meta joke here.",
+	"REALLY GREAT RPG 2 coming soon! I swear!",
+	"Having existential crises since 2013!",
+	"Capitalism is trash. Water is wet.",
+	"Some ferns are green, I'm in a mech,", // TODO: fix this rhyme.
+];
 
 // Once page loads completely...
 window.addEventListener("DOMContentLoaded", ()=>{
@@ -20,13 +35,18 @@ window.addEventListener("DOMContentLoaded", ()=>{
 			nav.classList.toggle("show-nav");
 		});
 		
-		// Unlink the page you're on
+		// Underline the page you're on
 		for (i = 0; i < navitems.length; i++) {
-			if (document.location.href == navitems[i].childNodes[0].href || document.location.pathname == navitems[i].childNodes[0].href) {
-				// navitems[i].innerHTML = navitems[i].childNodes[0].innerHTML;
+			if (navitems[i].childNodes[0].href == document.location.href.substring(0, navitems[i].childNodes[0].href.length)) {
 				navitems[i].classList.add("here");
 			}
 		}
+	}
+	
+	if (document.getElementById("footer-note")) {
+		footernote = document.getElementById("footer-note");
+		
+		footernote.innerHTML = footernotes[Math.floor(Math.random() * footernotes.length)];
 	}
 });
 
