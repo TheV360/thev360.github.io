c3f0254fcc55c0d801e832a39dd5564645abf3a0
V360 <v360code@gmail.com>
2018-11-30 17:13:09 -0500

    testing scrolling

--- js/home.js
+++ js/home.js
@@ -1,27 +1,50 @@
-var eggCodes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-var eggProgress = 0;
+// var eggCodes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+// var eggProgress = 0;
 
-document.addEventListener('keydown', (e)=>{
-	if (e.keyCode == eggCodes[eggProgress]) {
-		eggProgress++;
-		if (eggProgress == eggCodes.length) {
-			eggFunction();
-			eggProgress = 0;
-		}
-	} else {
-		eggProgress = 0;
-	}
+function getRootStyle(str) {
+	return getComputedStyle(document.documentElement).getPropertyValue(str);
+}
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+	var projectsButton = document.getElementById("projectsButton");
+	var firstProject = document.querySelector(".showcase");
+	
+	projectsButton.addEventListener("click", (e)=>{
+		// I'm sorry
+		var headerHeightInPixels = parseInt(getRootStyle("--header-height")) * parseInt(getRootStyle("font-size"));
+		
+		window.scrollTo({
+			top: firstProject.offsetTop - headerHeightInPixels,
+			left: document.documentElement.scrollLeft,
+			behavior: "smooth"
+		});
+	});
 });
 
-function eggFunction() {
-	const eggCredits = [
-		"Ooh! The Konami Code! I'll use this space to shoutout everyone I need to.",
-		"Shoutouts to the Petit Computer and SmileBASIC community, you guys are cool! Also, shoutouts to my family for always supporting my love of programming, even when it impacted my grades! Finally, shoutouts to BrianXP7, for making a Minecraft house with a room for me a while ago. Sadly, it's long been deleted, but I still appreciate it."
-	];
+// document.addEventListener("keydown", (e)=>{
+// 	if (e.keyCode == eggCodes[eggProgress]) {
+// 		eggProgress++;
+// 		if (eggProgress == eggCodes.length) {
+// 			eggFunction();
+// 			eggProgress = 0;
+// 		}
+// 	} else {
+// 		eggProgress = 0;
+// 	}
+// });
+
+// function eggFunction() {
+// 	const eggCredits = [
+// 		"Ooh! The Konami Code! I'll use this space to shoutout everyone I need to.",
+// 		"Shoutouts to the Petit Computer and SmileBASIC community, you guys are cool! Also, shoutouts to my family for always supporting my love of programming, even when it impacted my grades! Finally, shoutouts to BrianXP7, for making a Minecraft house with a room for me a while ago. Sadly, it's long been deleted, but I still appreciate it."
+// 	];
 	
-	var eggSelector = document.querySelectorAll("p");
+// 	var eggContainer = document.createElement("div");
+// 	eggContainer.classList.add("container");
 	
-	for (var i = 0; i < Math.min(eggSelector.length, eggCredits.length); i++) {
-		eggSelector[i].innerHTML = eggCredits[i];
-	}
-}
+// 	for (var i = 0; i < eggCredits.length; i++) {
+		
+// 	}
+	
+// 	'<div class="container"></div>';
+// }
