d635fa58e922f52b66cbed821a0239f8455ba6ea
V360 <v360code@gmail.com>
2018-11-30 18:36:04 -0500

    Nice

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ const footernotes = [
 	"Having existential crises since 2013!",
 	"Capitalism is trash. Water is wet.",
 	"Some ferns are green, I'm in a mech,", // TODO: fix this rhyme.
+	"Now contains 100% less JQuery!" // Just wanted to do a jab at JQuery, it's an alright library, but it's pretty bloated.
 ];
 
 // Once page loads completely...
